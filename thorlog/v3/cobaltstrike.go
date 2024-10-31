e226ef813e29ac2511fbbe2a612303e9cc50cc7d
gremat <50012463+gremat@users.noreply.github.com>
2024-10-31 16:08:09 +0100

    feat: add full beacon config to cobaltstrike struct

--- thorlog/v3/cobaltstrike.go
+++ thorlog/v3/cobaltstrike.go
@@ -9,4 +9,8 @@ type BeaconConfig struct {
 	Pipename         string `json:"pipename" textlog:"pipename"`
 	UserAgent        string `json:"user_agent" textlog:"user_agent"`
 	Proxy            string `json:"proxy" textlog:"proxy"`
+
+	// FullConfig is the full configuration of the beacon
+	// For now, it stays untyped until we refactor the parsing module.
+	FullConfig map[string]string `json:"full_config" textlog:"-"`
 }

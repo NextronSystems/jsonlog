ada12b5b19fe0b239f567761eff52af80edf1d5c
gremat <50012463+gremat@users.noreply.github.com>
2025-01-31 15:11:31 +0100

    fix: have only one module field occurrence in textlog

--- thorlog/common/event.go
+++ thorlog/common/event.go
@@ -18,7 +18,7 @@ const (
 type LogEventMetadata struct {
 	Time   time.Time `json:"time" textlog:"-"`
 	Lvl    LogLevel  `json:"level" textlog:"-"`
-	Mod    string    `json:"module" textlog:"module"`
+	Mod    string    `json:"module" textlog:"-"`
 	ScanID string    `json:"scanid" textlog:"scanid,omitempty"`
 	GenID  string    `json:"uid" textlog:"uid,omitempty"`
 	Source string    `json:"hostname" textlog:"-"`

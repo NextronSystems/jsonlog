95b350e83603b0bea294124b403a9af3d724687c
Max Altgelt <max.altgelt@nextron-systems.com>
2025-02-05 17:00:49 +0100

    feat: inline PE sieve report into process object
    Instead of reporting on the PE-Sieve report as a separate object,
    report it as part of the process.

--- thorlog/v3/pesieve.go
+++ thorlog/v3/pesieve.go
@@ -1,12 +1,6 @@
 package thorlog
 
-import (
-	"github.com/NextronSystems/jsonlog"
-)
-
 type PeSieveReport struct {
-	jsonlog.ObjectHeader
-
 	SuspiciousSections int `json:"suspicious_sections" textlog:"suspicious_sections"`
 	Replaced           int `json:"replaced" textlog:"replaced"`
 	HdrMod             int `json:"hdr_mod" textlog:"hdr_mod"`
@@ -17,18 +11,4 @@ type PeSieveReport struct {
 	Other              int `json:"other" textlog:"other"`
 	Skipped            int `json:"skipped" textlog:"skipped"`
 	Errors             int `json:"errors" textlog:"errors"`
-}
-
-func (PeSieveReport) reportable() {}
-
-const typePeSieveReport = "PE-Sieve report"
-
-func init() { AddLogObjectType(typePeSieveReport, &PeSieveReport{}) }
-
-func NewPeSieveReport() *PeSieveReport {
-	return &PeSieveReport{
-		ObjectHeader: jsonlog.ObjectHeader{
-			Type: typePeSieveReport,
-		},
-	}
 }
